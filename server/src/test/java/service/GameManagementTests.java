9313087cfa9f37efb04ee8a7753392c4b1f4ac43
Owen Werts <owen.werts@gmail.com>
2025-05-21 23:45:49 -0600

    Pasted Service Class tests from previous implementation.

---
+++ server/src/test/java/service/GameManagementTests.java
@@ -0,0 +1,203 @@
+package service;
+
+import chess.ChessGame;
+import dataaccess.DataAccessException;
+import model.Records.*;
+import model.UserData;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameManagementTests
+{
+	private static DAOManagement daoManager;
+	private static UserManagement userManager;
+	private static GameManagement gameManager;
+	String authToken;
+	int gameID;
+
+	@BeforeAll
+	public static void init()
+	{
+		daoManager = new DAOManagement();
+		userManager = new UserManagement(daoManager);
+		gameManager = new GameManagement(daoManager);
+	}
+
+	@BeforeEach
+	public void setup()
+	{
+		try
+		{
+			daoManager.clearApplication();
+
+			LoginResult result = userManager.register(new UserData("LickyFrog", "greenTreeFrog", "amazon@gmail.com"));
+			authToken = result.authToken();
+
+			NewGameResult result2 = gameManager.makeGame(new NewGameRequest(authToken, "TestGame"));
+			gameID = result2.gameID();
+		}
+		catch(DataAccessException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	@DisplayName("Make a Game")
+	public void makeGameTest()
+	{
+		daoManager.clearApplication();
+
+		try
+		{
+			daoManager.clearApplication();
+
+			LoginResult result = userManager.register(new UserData("LickyFrog", "greenTreeFrog", "amazon@gmail.com"));
+			String authToken = result.authToken();
+
+			NewGameResult actual = gameManager.makeGame(new NewGameRequest(authToken, "TestGame"));
+
+			assertNotNull(actual, "Result should not be null.");
+		}
+		catch(DataAccessException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	@DisplayName("Missing Game Name")
+	public void missingName()
+	{
+		Exception e = assertThrows(DataAccessException.class, () ->
+				gameManager.makeGame(new NewGameRequest(authToken, "")));
+
+		assertTrue(e.getMessage().contains("You must provide a game name"));
+	}
+
+	@Test
+	@DisplayName("Duplicate Game")
+	public void duplicateTest()
+	{
+		Exception e = assertThrows(DataAccessException.class, () ->
+				gameManager.makeGame(new NewGameRequest(authToken, "TestGame")));
+
+		assertTrue(e.getMessage().contains("already exists"));
+	}
+
+	@Test
+	@DisplayName("Invalid User Creates Game")
+	public void unauthorizedToMakeGameTest()
+	{
+		Exception e = assertThrows(DataAccessException.class, () ->
+				gameManager.makeGame(new NewGameRequest("FakeToken", "TestGame")));
+
+		assertTrue(e.getMessage().contains("no authorization"));
+	}
+
+	@Test
+	@DisplayName("LickyFrog Joins White")
+	public void joinWhiteTest()
+	{
+		try
+		{
+			gameManager.joinGame(new JoinGameRequest(authToken, ChessGame.TeamColor.WHITE, gameID));
+
+			assertEquals("LickyFrog", daoManager.getGames().getGame(gameID).whiteUsername());
+		}
+		catch(DataAccessException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	@DisplayName("LickyFrog Joins Black")
+	public void joinBlackTest()
+	{
+		try
+		{
+			gameManager.joinGame(new JoinGameRequest(authToken, ChessGame.TeamColor.BLACK, gameID));
+
+			assertEquals("LickyFrog", daoManager.getGames().getGame(gameID).blackUsername());
+		}
+		catch(DataAccessException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	@DisplayName("Invalid GameID to Join")
+	public void badGameIDTest()
+	{
+		Exception e = assertThrows(DataAccessException.class, () ->
+				gameManager.joinGame(new JoinGameRequest(authToken, ChessGame.TeamColor.WHITE, -1)));
+
+		assertTrue(e.getMessage().contains("A game with"));
+	}
+
+	@Test
+	@DisplayName("Invalid User Joins Game")
+	public void unauthorizedToJoinGameTest()
+	{
+		Exception e = assertThrows(DataAccessException.class, () ->
+				gameManager.joinGame(new JoinGameRequest("FakeToken", ChessGame.TeamColor.WHITE, gameID)));
+
+		assertTrue(e.getMessage().contains("no authorization"));
+	}
+
+	@Test
+	@DisplayName("Try to Join Occupied Team")
+	public void occupiedTeamTest()
+	{
+		try
+		{
+			LoginResult result = userManager.register(new UserData("SecondFrog", "1234", "nu@gmail.com"));
+			String secondAuthToken = result.authToken();
+
+			gameManager.joinGame(new JoinGameRequest(authToken, ChessGame.TeamColor.WHITE, gameID));
+
+			Exception e = assertThrows(DataAccessException.class, () ->
+					gameManager.joinGame(new JoinGameRequest(secondAuthToken, ChessGame.TeamColor.WHITE, gameID)));
+
+			assertTrue(e.getMessage().contains("Another user has already claimed"));
+		}
+		catch(DataAccessException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	@DisplayName("Successfully List Games")
+	public void listGamesTest()
+	{
+		try
+		{
+			//Setup Conditions
+			NewGameResult result2 = gameManager.makeGame(new NewGameRequest(authToken, "TestGame2"));
+			int gameID2 = result2.gameID();
+
+			//Setup Expected
+			ListedGame game1 = new ListedGame(gameID, null, null, "TestGame");
+			ListedGame game2 = new ListedGame(gameID2, null, null, "TestGame2");
+
+			//Calculate Actual
+			List<ListedGame> gameList = gameManager.listGames(authToken);
+
+			assertEquals(new HashSet<>(List.of(game1, game2)), new HashSet<>(gameList));
+		}
+		catch(DataAccessException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ server/src/test/java/service/UserManagementTests.java
@@ -0,0 +1,162 @@
+package service;
+
+import dataaccess.DataAccessException;
+import model.Records.LoginRequest;
+import model.Records.LoginResult;
+import model.UserData;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserManagementTests
+{
+	private static DAOManagement daoManager;
+	private static UserManagement userManager;
+
+	@BeforeAll
+	public static void init()
+	{
+		daoManager = new DAOManagement();
+		userManager = new UserManagement(daoManager);
+	}
+
+	@BeforeEach
+	public void setup()
+	{
+		try
+		{
+			daoManager.clearApplication();
+			userManager.register(new UserData("LickyFrog", "greenTreeFrog", "amazon@gmail.com"));
+		}
+		catch(DataAccessException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	@DisplayName("Successful User Registration")
+	public void lickyFrog()
+	{
+		daoManager.clearApplication();
+
+		try
+		{
+			LoginResult result = userManager.register(new UserData("LickyFrog", "greenTreeFrog", "amazon@gmail.com"));
+
+			assertNotNull(result, "Result should not be null.");
+			assertEquals("LickyFrog", result.username(), "Username should match the registered user.");
+			assertNotNull(result.authToken(), "AuthToken should not be null.");
+		}
+		catch(DataAccessException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	@DisplayName("Insufficient User Registration Information")
+	public void missingPassword()
+	{
+		UserData noPasswordStan = new UserData("Stan", "", "stan.lee@hotmail.com");
+
+		Exception e = assertThrows(DataAccessException.class, () ->	userManager.register(noPasswordStan));
+
+		assertTrue(e.getMessage().contains("You must provide a username, password, & email."));
+	}
+
+	@Test
+	@DisplayName("Duplicate User")
+	public void secondFrog()
+	{
+		UserData secondFrog = new UserData("LickyFrog", "brownTreeFrog", "florida@gmail.com");
+
+		Exception exception = assertThrows(DataAccessException.class, () ->	userManager.register(secondFrog));
+
+		assertTrue(exception.getMessage().contains("already exists"));
+	}
+
+	@Test
+	@DisplayName("Successful Login")
+	public void loginTest()
+	{
+		try
+		{
+			LoginResult result = userManager.login(new LoginRequest("LickyFrog", "greenTreeFrog"));
+
+			assertNotNull(result);
+			assertEquals("LickyFrog", result.username());
+			assertNotNull(result.authToken());
+		}
+		catch(DataAccessException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	@DisplayName("Wrong Password")
+	public void badPasswordTest()
+	{
+		LoginRequest wrongPassword = new LoginRequest("LickyFrog", "brownTreeFrog");
+
+		Exception e = assertThrows(DataAccessException.class, () ->	userManager.login(wrongPassword));
+		assertTrue(e.getMessage().contains("Incorrect password for LickyFrog"));
+	}
+
+	@Test
+	@DisplayName("Non-existent User")
+	public void noUserTest()
+	{
+		LoginRequest unregisteredUser = new LoginRequest("JohnDoe", "brownTreeFrog");
+
+		Exception e = assertThrows(DataAccessException.class, () ->	userManager.login(unregisteredUser));
+		assertTrue(e.getMessage().contains("User JohnDoe does not exist."));
+	}
+
+	@Test
+	@DisplayName("Successful Logout")
+	public void logoutTest()
+	{
+		daoManager.clearApplication();
+
+		try
+		{
+			LoginResult result = userManager.register(new UserData("Stan", "1324", "stan.lee@hotmail.com"));
+			String authToken = result.authToken();
+
+			userManager.logout(authToken);
+
+			assertTrue(daoManager.getAuthorizations().isEmpty(), "Authorizations isn't empty.");
+		}
+		catch(DataAccessException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	@DisplayName("Double Logout")
+	public void doubleLogoutTest()
+	{
+		daoManager.clearApplication();
+
+		try
+		{
+			LoginResult result = userManager.register(new UserData("Stan", "1324", "stan.lee@hotmail.com"));
+			String authToken = result.authToken();
+
+			userManager.logout(authToken);
+
+			Exception e = assertThrows(DataAccessException.class, () ->	userManager.logout(authToken));
+			assertTrue(e.getMessage().contains("no authorization token"));
+		}
+		catch(DataAccessException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+}
