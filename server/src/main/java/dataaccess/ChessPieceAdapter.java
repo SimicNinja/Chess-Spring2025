d9e50ed1e9ae22a9b98c040ce2fc45359f1ec6ea
Owen Werts <owen.werts@gmail.com>
2025-05-28 12:02:31 -0600

    Paste in ChessPieceAdapter class from previous implementation for proper deserialization of ChessPieces.

---
+++ server/src/main/java/dataaccess/ChessPieceAdapter.java
@@ -0,0 +1,23 @@
+package dataaccess;
+
+import chess.ChessGame;
+import chess.ChessPiece;
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+
+public class ChessPieceAdapter implements JsonDeserializer<ChessPiece>
+{
+	@Override
+	public ChessPiece deserialize(JsonElement json, Type type, JsonDeserializationContext context)
+			throws JsonParseException
+	{
+		JsonObject pieceObject = json.getAsJsonObject();
+
+		ChessGame.TeamColor color = context.deserialize(pieceObject.get("color"), ChessGame.TeamColor.class);
+		ChessPiece.PieceType pieceType = context.deserialize(pieceObject.get("type"), ChessPiece.PieceType.class);
+		boolean hasMoved = pieceObject.get("hasMoved").getAsBoolean();
+
+		return new ChessPiece(color, pieceType, hasMoved);
+	}
+}
